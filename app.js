3bd7c68cea0d84271f7a92a06146f6b322c41f3e
djknit <djknit@gmail.com>
2018-09-21 00:12:38 -0500

    added comments

--- app.js
+++ app.js
@@ -12,23 +12,28 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 var database = firebase.database();
 
+// create variables to hold user input
 var name = "";
 var role = "";
 var startDate = "";
 var monthlyRate = "";
 
+// Grab the input when the form is submitted
 $("#submit").on("click", function(event) {
-    console.log("button clicked");
     
+    // Keep the page from reloading
     event.preventDefault();
 
+    // Store the input values
     name = $("#employee-name").val().trim();
     role = $("#employee-role").val().trim();
     startDate = $("#employee-start-date").val().trim();
     monthlyRate = $("#employee-monthly-rate").val().trim();
 
+    // Convert date to a MomentJS object
     var startDateValue = moment(startDate, "MM/DD/YYYY").valueOf();
 
+    // Store info in database
     database.ref().push({
         name: name,
         role: role,
@@ -36,25 +41,33 @@ $("#submit").on("click", function(event) {
         monthlyRate : monthlyRate,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+
+    // Clear input fields
+    $(".form-control").val("");
 });
 
-// Firebase watcher .on("child_added"
+// Update the page whenever a new employee is added to the database
 database.ref().on("child_added", function(snapshot) {
+    
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
 
-    console.log(sv.name);
-    console.log(sv.role);
-    console.log(sv.startDate);
-    console.log(sv.monthlyRate)
+    // console.log(sv);
+    // console.log(sv.name);
+    // console.log(sv.role);
+    // console.log(sv.startDate);
+    // console.log(sv.monthlyRate)
 
+    // Create MomentJS object from date
     var dateMoment = moment(sv.startDate);
 
     // Calculate months worked
     var monthsWorked = moment().diff(dateMoment, "months");
 
+    // Create table row for new employee info
     var newTableRow = $("<tr>");
     
+    // Create table data elements for employee info
     var nameData = $("<td>");
     nameData.html(sv.name);
 
@@ -70,9 +83,10 @@ database.ref().on("child_added", function(snapshot) {
     var monthsWorkedData = $("<td>");
     monthsWorkedData.html(monthsWorked);
 
-
+    // Attach the data elements to the row
     newTableRow.append(nameData).append(roleData).append(startDateData).append(monthlyRateData);
 
+    // Attach the row to the table on the page
     $("#employee-data").append(newTableRow);
 
     // Handle the errors
